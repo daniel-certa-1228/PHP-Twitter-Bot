176a295fcec9a58a213f60928f1ce66a9756ea9b
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-21 14:13:20 -0600

    db connected; ready to test deploy

--- henleyBot.php
+++ henleyBot.php
@@ -5,6 +5,18 @@ require 'lyrics.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+$eoi_lyrics = Lyrics::$lyricsArray;//get the lyrics from the Lyrics class
+
+//connect to dabase
+$c = pg_connect("dbname=eoi_counter")
+    or die("Could not connect: " . pg_last_error());
+$query = "SELECT counter FROM counter";
+$result = pg_query($query)
+    or die("Query failed: " . pg_last_error());
+$count = pg_fetch_result($result,0,0);//get the current count from the database
+
+// print("$count\n");
+
 $consumerKey = getenv('TWITTER_CONSUMER_KEY_HB'); // Consumer Key
 $consumerSecret = getenv('TWITTER_CONSUMER_SECRET_HB'); // Consumer Secret
 $accessToken = getenv('TWITTER_ACCESS_TOKEN_HB'); // Access Token
@@ -12,9 +24,20 @@ $accessTokenSecret = getenv('TWITTER_ACCESS_TOKEN_SECRET_HB'); // Access Token S
 
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
-// $status = $connection->post("statuses/update", array("status" => "I'm posting a test tweet!"));
-// var_dump($status);
-$eoi_lyrics = Lyrics::$lyricsArray;
-var_dump($eoi_lyrics[0]);
+$status = $connection->post("statuses/update", array("status" => "$eoi_lyrics[$count]"));
+
+var_dump($status);//print the status from twitter to the console
+
+$count += 1;//increment the counter
+
+// print("$count\n");
+
+if ($count == count($eoi_lyrics)) {
+    $set = pg_query("UPDATE counter SET counter = 0");
+}  else  {
+    $set = pg_query("UPDATE counter SET counter = '$count'");
+};
+
+pg_close($c); //close the database connection
 
 ?>
