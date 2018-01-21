815d7aacf0a99a424ae258e5bb449969fa163015
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-21 14:37:39 -0600

    indents

--- henleyBot.php
+++ henleyBot.php
@@ -1,43 +1,43 @@
 <?php
 
-require "vendor/abraham/twitteroauth/autoload.php";
-require 'lyrics.php';
+    require "vendor/abraham/twitteroauth/autoload.php";
+    require 'lyrics.php';
 
-use Abraham\TwitterOAuth\TwitterOAuth;
+    use Abraham\TwitterOAuth\TwitterOAuth;
 
-$eoi_lyrics = Lyrics::$lyricsArray;//get the lyrics from the Lyrics class
+    $eoi_lyrics = Lyrics::$lyricsArray;//get the lyrics from the Lyrics class
 
-//connect to dabase
-$c = pg_connect("dbname=eoi_counter")
-    or die("Could not connect: " . pg_last_error());
-$query = "SELECT counter FROM counter";
-$result = pg_query($query)
-    or die("Query failed: " . pg_last_error());
-$count = pg_fetch_result($result,0,0);//get the current count from the database
+    //connect to dabase
+    $c = pg_connect("dbname=eoi_counter")
+        or die("Could not connect: " . pg_last_error());
+    $query = "SELECT counter FROM counter";
+    $result = pg_query($query)
+        or die("Query failed: " . pg_last_error());
+    $count = pg_fetch_result($result,0,0);//get the current count from the database
 
-// print("$count\n");
+    // print("$count\n");
 
-$consumerKey = getenv('TWITTER_CONSUMER_KEY_HB'); // Consumer Key
-$consumerSecret = getenv('TWITTER_CONSUMER_SECRET_HB'); // Consumer Secret
-$accessToken = getenv('TWITTER_ACCESS_TOKEN_HB'); // Access Token
-$accessTokenSecret = getenv('TWITTER_ACCESS_TOKEN_SECRET_HB'); // Access Token Secret
+    $consumerKey = getenv('TWITTER_CONSUMER_KEY_HB'); // Consumer Key
+    $consumerSecret = getenv('TWITTER_CONSUMER_SECRET_HB'); // Consumer Secret
+    $accessToken = getenv('TWITTER_ACCESS_TOKEN_HB'); // Access Token
+    $accessTokenSecret = getenv('TWITTER_ACCESS_TOKEN_SECRET_HB'); // Access Token Secret
 
-$connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+    $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
-$status = $connection->post("statuses/update", array("status" => "$eoi_lyrics[$count]"));
+    $status = $connection->post("statuses/update", array("status" => "$eoi_lyrics[$count]"));
 
-var_dump($status);//print the status from twitter to the console
+    var_dump($status);//print the status from twitter to the console
 
-$count += 1;//increment the counter
+    $count += 1;//increment the counter
 
-// print("$count\n");
+    // print("$count\n");
 
-if ($count == count($eoi_lyrics)) {
-    $set = pg_query("UPDATE counter SET counter = 0");
-}  else  {
-    $set = pg_query("UPDATE counter SET counter = '$count'");
-};
+    if ($count == count($eoi_lyrics)) {
+        $set = pg_query("UPDATE counter SET counter = 0");
+    }  else  {
+        $set = pg_query("UPDATE counter SET counter = '$count'");
+    };
 
-pg_close($c); //close the database connection
+    pg_close($c); //close the database connection
 
 ?>
