9f789881a3f2cc8ff2e97cd8d1388c2b5d76cf5e
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-22 10:03:29 -0600

    added comments

--- henleyBot.php
+++ henleyBot.php
@@ -5,7 +5,8 @@
     use Abraham\TwitterOAuth\TwitterOAuth;
 
     $eoi_lyrics = Lyrics::$lyricsArray;//get the lyrics from the Lyrics class
-
+    //the counter that keeps track of which line the program is on is stored in a database
+    //get the database login info from env
     $dbopts = parse_url(getenv('DATABASE_URL'));
     $dbname = ltrim($dbopts["path"],'/');
     //connect to dabase
@@ -15,24 +16,22 @@
     $result = pg_query($query)
         or die("Query failed: " . pg_last_error());
     $count = pg_fetch_result($result,0,0);//get the current count from the database
-
     // print("$count\n"); //inspect the count if necessary
     
     $consumerKey = getenv('TWITTER_CONSUMER_KEY_HB'); // Consumer Key
     $consumerSecret = getenv('TWITTER_CONSUMER_SECRET_HB'); // Consumer Secret
     $accessToken = getenv('TWITTER_ACCESS_TOKEN_HB'); // Access Token
     $accessTokenSecret = getenv('TWITTER_ACCESS_TOKEN_SECRET_HB'); // Access Token Secret
-
+    //connect to Twitter using credentials from env
     $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-
+    // tweet the appropriate line
     $status = $connection->post("statuses/update", array("status" => "$eoi_lyrics[$count]"));
 
     var_dump($status);//print the status from twitter to the console
 
     $count += 1;//increment the counter
-
     // print("$count\n"); //inspect the incremented count if necessary
-
+    //save the incremented count to the db or reset to zero if appropriate
     if ($count == count($eoi_lyrics)) {
         $set = pg_query("UPDATE counter SET counter = 0");
     }  else  {
